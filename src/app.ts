fd3dc7d5a56f242acb389d951dc8f54148288774
Matteo Valenzi <matteo.valenzi@outlook.com>
2024-05-20 21:11:21 +0200

    initial

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("Ciao!");
+import { config } from "dotenv";
+config();
+
+import express, { Request, Response } from "express";
+import morgan from "morgan";
+import * as db from "./db";
+
+const app = express();
+const port = Number(process.env.PORT) || 3000;
+
+// middlewares
+app.use(morgan("dev"));
+
+app.get("/", async (req: Request, res: Response) => {
+    let posts = await db.getPosts();
+    res.json(posts);
+});
+
+app.listen(port, () => console.log(`Server in ascolto su http://localhost:${port}`));
